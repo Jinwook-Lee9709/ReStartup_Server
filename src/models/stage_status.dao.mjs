bb0b5f7218a88229ac285e5daae48bceb266c1fb
Jinwook-Lee9709 <wooki9709@gmail.com>
2025-04-28 10:32:13 +0900

    dao 수정

--- src/models/stage_status.dao.mjs
+++ src/models/stage_status.dao.mjs
@@ -1,7 +1,7 @@
 import { db } from '../config/db.mjs';
 
 const GET_ALL_QUERY = 'SELECT * FROM stage_status WHERE uuid = ?';
-const SAVE_THEME_INFO_QUERY = 'INSERT INTO stage_status (uuid, theme, is_cleared, last_claim, manager_count) VALUES (?, ?, ?, ?)';
+const SAVE_THEME_INFO_QUERY = 'INSERT INTO stage_status (uuid, theme, is_cleared, last_claim, manager_count) VALUES (?, ?, ?, ?, ?)';
 
 export const GetStatus ={
     all: async (uuid) => {
