497ddf1c464932b732dbbd207ffa02fc7a33cc35
Jinwook-Lee9709 <wooki9709@gmail.com>
2025-04-28 09:50:22 +0900

    로그 추가

--- src/controllers/stage_status.ctrl.mjs
+++ src/controllers/stage_status.ctrl.mjs
@@ -12,7 +12,8 @@ export const process = {
     saveStatus: async(req, res) => {
         await handleRequest(async() => {
             const uuid = req.uuid;
-            console.log(req.body);
+            console.log(req);
+            console.log(req.body.length);
             console.log(uuid);
             const info = req.body.info;
             const result = await SaveStatus.single(uuid, info);
