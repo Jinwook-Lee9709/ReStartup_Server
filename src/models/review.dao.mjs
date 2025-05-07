c92f2788f6905cd280d16a55deecfe8e0c57ffb0
Jinwook-Lee9709 <wooki9709@gmail.com>
2025-05-07 10:54:42 +0900

    로깅

--- src/models/review.dao.mjs
+++ src/models/review.dao.mjs
@@ -1,9 +1,7 @@
 import { db } from '../config/db.mjs';
 
 const INSERT_REVIEW_PROCEDURE = `CALL InsertReview(?, ?, ?, ?)`;
-const DELETE_REVIEW_PROCEDURE  = `CALL DeleteMiddleAndReorder(?, ?, @success_flag);
-            SELECT @success_flag AS success_flag;
-`;
+const DELETE_REVIEW_PROCEDURE  = `CALL DeleteMiddleAndReorder(?, ?, @success_flag)`;
 const GET_ALL_REVIEW_QUERY = `SELECT * FROM review WHERE uuid = ?`;
 
 export const Review = {
@@ -20,11 +18,12 @@ export const Review = {
     delete: async (uuid, orderIndex)=>
     {
         try{
-            const [resultSets] = await db.query(DELETE_REVIEW_PROCEDURE, [uuid, orderIndex]);
-            const successFlag = resultSets[1][0].success_flag;
+            await db.query(DELETE_REVIEW_PROCEDURE, [uuid, orderIndex]);
+            const [[row]] = await db.query('SELECT @success_flag AS success_flag');
+            const successFlag = row.success_flag;
 
-            console.log('Success Flag:', successFlag);
-            console.log('Result Sets:', resultSets);
+            console.log('row:', successFlag);
+
             return successFlag === 1;
 
         }catch(error){
